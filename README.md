cca7262ec4509cb8372335b32b48636108ee6316
Nigel <barrowjr0@gmail.com>
2024-05-17 14:52:28 -0400

    Add README

---
+++ README.md
@@ -0,0 +1,15 @@
+# News From Space
+
+News site built with Nextjs using The Space Devs' Spaceflight News and Launch Library 2 APIs
+
+**Link:** https://www.newsfromspace.site/
+
+## Built With
+
+- [React](https://react.dev/)
+- [NextJs](https://nextjs.org/)
+- [Spaceflight News API](https://thespacedevs.com/snapi)
+- [Launch Library 2](https://thespacedevs.com/llapi)
+- [Supabase](https://supabase.com/)
+- [NextUI](https://nextui.org/)
+- [Tailwind](https://tailwindcss.com/)
--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -208,7 +208,7 @@ export default async function HomePage() {
                     <Card className="flex min-h-52 w-full flex-row py-2 sm:h-full ">
                       <Image
                         alt="Blog image"
-                        className="z-0 ml-2 h-full w-44 flex-shrink rounded-xl object-cover sm:w-44 sm:flex-1 lg:w-56"
+                        className="z-0 ml-2 h-full max-h-48 w-44 flex-shrink rounded-xl object-cover sm:w-44 sm:flex-1 lg:w-56"
                         src={blog.image_url}
                       />
 
