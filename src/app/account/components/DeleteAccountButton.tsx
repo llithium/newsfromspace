fc53744ef1a13383cf59b997876380c069051c57
Nigel <barrowjr0@gmail.com>
2024-05-19 07:59:45 -0400

    Add confirm dialog when deleting account

--- src/app/account/components/DeleteAccountButton.tsx
+++ src/app/account/components/DeleteAccountButton.tsx
@@ -1,20 +1,58 @@
 "use client";
-import { Button } from "@nextui-org/react";
+import {
+  Button,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/react";
 import { deleteUser } from "../actions";
 
 const DeleteAccountButton = () => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   return (
-    <Button
-      className="text-sm font-medium"
-      size="sm"
-      variant="light"
-      onPress={() => {
-        deleteUser();
-      }}
-      color="danger"
-    >
-      Delete Account
-    </Button>
+    <>
+      <Button
+        className="text-sm font-medium"
+        size="sm"
+        variant="light"
+        color="danger"
+        onPress={onOpen}
+      >
+        Delete Account
+      </Button>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Delete Account
+              </ModalHeader>
+              <ModalBody>
+                <p>Confirm that you wish to delete your account</p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button
+                  className="text-sm font-medium"
+                  variant="light"
+                  onPress={() => {
+                    deleteUser();
+                  }}
+                  color="danger"
+                >
+                  Delete Account
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
